0bbe4568db79c7f4d37f0f9c3f4454b4c4179951
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-02-26 15:28:31 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      hometown: ''
+  }
+}
+
+listenNewUserUsername = (event) => {
+  this.setState({
+    username: event.target.value
+  })
+
+}
+
+listenNewUserHometown = (event) => {
+  this.setState({
+    hometown: event.target.value
+  })
+}
+
+handleAddNewUser = (event) => {
+  event.preventDefault()
+  this.props.store.dispatch({
+    type: 'ADD_USER',
+    user: {
+      username: this.state.username,
+      hometown: this.state.hometown,
+    }
+  })
+}
+
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={(event) => this.handleAddNewUser(event)}>
+          <input type="text" value={this.state.username} onChange = {this.listenNewUserUsername} />
+          <input type="text" value={this.state.hometown} onChange={this.listenNewUserHometown} />
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,13 @@
-export default function manageUsers(state, action){
+
+
+export default function manageUsers(state = {
+  users: []
+}, action){
+  switch(action.type) {
+    case 'ADD_USER':
+      const user = Object.assign({}, action.user)
+      return {users: state.users.concat(user) }
+    default:
+      return state;
+  }
 }
